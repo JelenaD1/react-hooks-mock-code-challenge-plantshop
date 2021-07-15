dd4f6d8ed3cebb8ced54e7e4bdb3cd1b78470994
Jelena <jelena.durovic@powerhrg.com>
2021-07-15 14:36:14 -0400

    commit tree bc0e655a220f13924b9ae218212f41004946462e

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const API = ("http://localhost:6001/plants")
+
+  useEffect(() => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => setPlants(data))
+
+  }, [])
+
+  const handleAddPlant = (newPlant) => {
+    fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newPlant)
+    })
+     .then(resp => resp.json())
+     .then(data => setPlants([...plants, data]))
+
+  }
+  
+  // const handlePlantSearch = (searchTerm) => {
+  //   const filteredPlants = plants.filter((plant => plant.name.toLowerCase().includes(searchTerm.toLowerCase())))
+  //   setPlants(filteredPlants)
+
+  // }
+
+  const plantsToDisplay = plants.filter(plant => plant.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search  
+               searchTerm={searchTerm}
+               setSearchTerm={setSearchTerm} />
+      <PlantList plants={plantsToDisplay}
+      
+      />
     </main>
   );
 }
