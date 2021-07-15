05a733ef4444aaa8cb24174b2cdf6b3bca7420cf
Jelena <jelena.durovic@powerhrg.com>
2021-07-15 16:22:52 -0400

    done

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant, onClickDelete}) {
+  
+
+  const [inStock, setInStock] = useState(true)
+
+  const[currentPrice, setCurrentPrice] = useState(plant.price)
+
+  const handleClick = () => {
+    setInStock(!inStock)
+  }
+  
+
+  const handleDelete = () => {
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "DELETE"
+    })
+    onClickDelete(plant.id)
+
+  }
+  const handlePriceChange = (e) => {
+    setCurrentPrice(e.target.value)
+  }
+
+  const newPriceSubmit = (e) => {
+    e.preventDefault()
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        price: currentPrice
+      })
+   
+    })
+   }
+  
+
+  
+  
+
   return (
-    <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+    <li  className="card">
+      <img src={plant.image} alt={"plant name"} />
+      <h4>{plant.name}</h4>
+      <form onSubmit={newPriceSubmit}>
+      <input 
+        value={currentPrice}
+        onChange={handlePriceChange}
+        type="number" 
+        name="price" 
+        step="0.01" 
+        placeholder={currentPrice} />
+        <input 
+        type="submit"
+        value="submit">
+        </input>
+      </form>
+      {/* //</form><p>Price: {plant.price}</p> */}
+     
+      {inStock ? (
+        <button onClick={handleClick} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
+      <button onClick={handleDelete}>Delete Plant</button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,61 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const API = ("http://localhost:6001/plants")
+
+  useEffect(() => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => setPlants(data))
+
+  }, [])
+
+  const handleAddPlant = (newPlant) => {
+    fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newPlant)
+    })
+     .then(resp => resp.json())
+     .then(data => setPlants([...plants, data]))
+
+  }
+  
+  
+
+  const plantsToDisplay = plants.filter(plant => plant.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  const deletePlant = (id) => {
+    const updatedPlantList = plants.filter(plant => plant.id !== id)
+    setPlants(updatedPlantList)
+    debugger
+
+
+  }
+
+  
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search  
+               searchTerm={searchTerm}
+               setSearchTerm={setSearchTerm} />
+      <PlantList plants={plantsToDisplay}
+                 onClickDelete={deletePlant}
+                 
+      
+      />
     </main>
   );
 }
